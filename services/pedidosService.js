3a5c017dac539a657fe5a95dd53790584da981df
Jorge Herrera Robledo <jherrera@makita.cl>
2024-07-01 08:28:12 -0400

    credenciales produccion

--- services/pedidosService.js
+++ services/pedidosService.js
@@ -111,8 +111,8 @@ async function facturarPedidos(item){
        const response = await axios.post( url, data ,{
             headers: {
                 'Content-Type': 'application/json',
-                'Access-Application-Key': '3d137dea1d13220aa9a10ee57d69f6b30d247f28',
-                'Access-Env': 'HOMOLOGATION',
+                'Access-Application-Key': '588b56a33c722da5e49170a311e872d9ee967291',
+                'Access-Env': 'PRODUCTION',
                 'X-Custom-Header': 'value'
             },
         });
